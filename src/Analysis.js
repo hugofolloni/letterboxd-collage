d8ed96c92868c2ae9ea2f577685e2e7a851b3bdc
hugofolloni <hugofollogua07@gmail.com>
2023-12-24 00:01:49 -0300

    Its working, but not perfect

--- src/Analysis.js
+++ src/Analysis.js
@@ -1,9 +1,8 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import axios from 'axios';
-import cheerio, { text } from 'cheerio';
+import cheerio from 'cheerio';
 
 const Analysis = () => {
-    const [textList, setTextList] = useState([])
     const [posterList, setPosterList] = useState([])
     const [username, setUsername] = useState('')
     const [year, setYear] = useState(2023)
@@ -11,6 +10,7 @@ const Analysis = () => {
     const scrappy = async (user, year, page) => {
 
         var texts = []
+        var years = []
         const proxy = 'https://thingproxy.freeboard.io/fetch/'
         const pageUrl = `https://letterboxd.com/${user}/films/diary/for/${year}`
 
@@ -23,33 +23,48 @@ const Analysis = () => {
         console.log(film_number)
         const pages = Math.ceil(film_number / 50)
 
-        $('.headline-3 > a').each((_idx, el) => {
-            const bodyPart = $(el).text()
-            texts.push(bodyPart)
-        });
+        while(texts.length < film_number){
 
-        if(pages > 1){
-            for(let i = 2; i < pages + 1; i++){
-    
-                const proxy = 'https://thingproxy.freeboard.io/fetch/'
-                const { data } = await axios.get(`${proxy}https://letterboxd.com/${user}/films/diary/for/${year}/page/${i}`);
-                const $ = cheerio.load(data);
+            $('.headline-3 > a').each((_idx, el) => {
+                const bodyPart = $(el).text()
+                texts.push(bodyPart)
+            });
 
-                $('.headline-3 > a').each((_idx, el) => {
-                    const bodyPart = $(el).text()
-                    texts.push(bodyPart)
-                });
+            $(".td-released > span").each((_idx, el) => {
+                const bodyPart = $(el).text()
+                years.push(bodyPart)
+            });
+
+            if(pages > 1){
+                for(let i = 2; i < pages + 1; i++){
+                    const proxy = 'https://thingproxy.freeboard.io/fetch/'
+                    const { data } = await axios.get(`${proxy}https://letterboxd.com/${user}/films/diary/for/${year}/page/${i}`);
+                    const $ = cheerio.load(data);
+
+                    $('.headline-3 > a').each((_idx, el) => {
+                        const bodyPart = $(el).text()
+                        texts.push(bodyPart)
+                    });
+                    
+                    $(".td-released > span").each((_idx, el) => {
+                        const bodyPart = $(el).text()
+                        years.push(bodyPart)
+                    });
+                }
             }
         }
 
-        setTextList(texts)
-        console.log(textList)
+        console.log(texts)
+        return [texts, years]
+    }
 
+    const getPosters = (texts, years) => {
         var posters = []
+        console.log(texts.length)
        
-        
         for(let i = 0; i < texts.length; i++){
-            const url = `https://api.themoviedb.org/3/search/movie?query=${texts[i]}&include_adult=false&language=en-US&page=1`;
+            console.log(i)
+            const url = `https://api.themoviedb.org/3/search/movie?query=${texts[i]}&year=${years[i]}&include_adult=false&language=en-US&page=1`;
             
             const options = {
                 method: 'GET',
@@ -64,29 +79,45 @@ const Analysis = () => {
             .then(json => {
                 if(json.results.length > 0){
                     if(json.results[0].poster_path !== undefined){
-                        posters.push({source: 'https://image.tmdb.org/t/p/w92' + json.results[0].poster_path})
+                        console.log(json.results[0].poster_path)
+                        posters.push(`https://image.tmdb.org/t/p/w92${json.results[0].poster_path}`)
                     }}
                 }
                 )
             .catch(err => console.error('error:' + err));
         }
 
+        return posters
 
-        setPosterList(posters.reverse())
-        console.log(posterList)
     }
+
+
+    const getCollage = async (username, year, page) => {
+        var texts = await scrappy(username, year, page)
+        var movies = getPosters(texts[0], texts[1])
+        setTimeout(() => {
+            const possible_lenghts = [7, 6, 5, 8, 9, 11, 13]
+            var poster_row = 7
+            for(let i = 0; i < possible_lenghts.length; i++){
+                if(movies.length % possible_lenghts[i] === 0){
+                    poster_row = possible_lenghts
+                    break
+                }
+            }
+            console.log(poster_row)
+            setPosterList(movies)
+        }, 4000)
+    }
+    
 
     return ( 
         <div>
             <input type="text" placeholder="Usuário" onChange={(e) => setUsername(e.target.value)} value={username} />
-            <button onClick={() => scrappy(username, year, 1)}>Buscar</button>
+            <button onClick={() => getCollage(username, year, 1)}>Buscar</button>
+            <br></br>
             {
-                posterList.map(poster => (<img src={poster.source} alt=''/>))
+                posterList.reverse().map(poster => (<img src={poster} alt=''/>))
             }
-            {
-                posterList.map(poster => <p>{poster.source}</p>)
-            }
-
         </div>
         );
 
